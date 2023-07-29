cc11091e2a9363fc75119c73e78baf2e5ee4f13a
alecasanchez <alecsanchez162@gmail.com>
2023-07-29 18:01:21 -0500

    everything is working

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Teemup
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
